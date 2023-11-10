09f33431b739d8d584e759adba2a4464d44cf5dc
Vikki9121 <vignesh24csengineer@gmail.com>
2023-11-10 09:32:25 +0530

    first commit

---
+++ Assets/Script/Rotation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject PlanetObject;
+    public Vector3 RotationVector;
+
+    private void Update()
+    {
+        PlanetObject.transform.Rotate(RotationVector * Time.deltaTime);
+    }
+}
---
+++ Assets/Script/SceneController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public void SwitchScenes(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
